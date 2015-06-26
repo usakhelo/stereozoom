0b773b337965d221a8d538eeb654f49291b2d09b
Sergo <sergo.pogosyan@gmail.com>
2015-06-25 23:51:13 -0400

    dragging works

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ function init() {
 	container = document.getElementById("content");
 	left_div = container.getElementsByClassName("left")[0];
 	right_div = container.getElementsByClassName("right")[0];
-	left_div.addEventListener( 'click', onViewClick, false );
+	// left_div.addEventListener( 'click', onViewClick, false );
 	left_div.addEventListener( 'wheel', onViewWheel, false );
-	right_div.addEventListener( 'click', onViewClick, false );
+	// left_div.addEventListener( 'mousemove', onMouseMove, false );
+	left_div.addEventListener("dragstart", function() { console.log(event.button);return false; }, false );
+	left_div.addEventListener("mousedown", onMouseDown, false );
+	// right_div.addEventListener( 'click', onViewClick, false );
 	right_div.addEventListener( 'wheel', onViewWheel, false );
 	loadImage();
 }
@@ -39,13 +42,48 @@ function onImageLoad(event) {
 }
 
 function onViewClick(event) {
+	console.log(event.button);
 	var local_rect = event.target.getBoundingClientRect();
-	var click_x = event.clientX - local_rect.x;
-	var click_y = event.clientY - local_rect.y;
+	var click_x = event.clientX - local_rect.left;
+	var click_y = event.clientY - local_rect.top;
 }
 
+var start_x, start_y;
+function onMouseMove(event) {
+	console.log(event);
+	var local_rect = event.target.getBoundingClientRect();
+	var click_x = event.clientX - local_rect.left;
+	var click_y = event.clientY - local_rect.top;
+	var dist_x = click_x - start_x;
+	var dist_y = click_y - start_y;
+	start_x = click_x;
+	start_y = click_y;
+	console.log(dist_x);
+	image_left = image_left + dist_x;
+	image_top = image_top + dist_y;
+	displayImage();
+}
+
+function onMouseUp(event) {
+	console.log(event);
+	event.target.removeEventListener( 'mousemove', onMouseMove );
+	event.target.removeEventListener( 'mouseup', onMouseUp );
+}
+
+function onMouseDown(event) {
+	console.log(event);
+	var local_rect = event.target.getBoundingClientRect();
+	start_x = event.clientX - local_rect.left;
+	start_y = event.clientY - local_rect.top;
+	if (event.buttons == 1) {
+		event.target.addEventListener( 'mousemove', onMouseMove, false );
+		event.target.addEventListener( 'mouseup', onMouseUp, false );
+	}
+}
+		
+
 function onViewWheel(evt) {
-	console.log(evt.deltaY);
+	// console.log(evt.deltaY);
 	var target_rect = evt.target.getBoundingClientRect();
 	var target_width = target_rect.width;
 	var click_x = evt.clientX - target_rect.left;
@@ -60,13 +98,13 @@ function onViewWheel(evt) {
 	if (image_top > 0) image_top = 0;
 	if (image_right < (target_width * 2)) image_right = (target_width * 2);
 
-	console.log(scale);
-	displayImage(target_width);
+	// console.log(scale);
+	displayImage();
 
 	evt.preventDefault();
 }
 
-function displayImage(div_width) {
+function displayImage() {
 	var image_width = (image_right - image_left);
 	right_div.style.backgroundSize = left_div.style.backgroundSize = image_width.toString() + "px auto";
 	left_div.style.backgroundPosition = image_left.toString() + "px " + image_top.toString() + "px";
